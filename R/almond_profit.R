d47df070c1b417bafffca9d4dd38d709a50d2cc7
kristingill <kristin_gill@ucsb.edu>
2022-04-14 12:17:45 -0700

    almond profit

---
+++ R/almond_profit.R
@@ -0,0 +1,24 @@
+#' Almond Yield
+#'
+#' This function computes profit from selling almonds based on almond yield, price, discount rate, and acres.
+#' @param almond_yield (ton/acre)
+#' @param discount_rate (default = 0.12)
+#' @param price ($/ton)
+#' @param acres
+#' @param year
+#' @author Yutian Fang, Kristin Gill, and Grace Lewin
+#' @return almond profit ($)
+
+compute_profit_almond = function(yield, year, price, discount=0.12, acres) {
+  
+  scen = seq(from=1, to=length(yield))
+  
+  yearprofit <- data.frame(scen=scen, yield=yield, year=year)
+  
+  yearprofit$net <-  yearprofit$yield*price
+  
+  Yearprofit <- yearprofit %>% 
+    mutate(netpre = compute_NPV(value=net, time=year-year[1], discount=discount ))
+  
+  return(yearprofit)
+}
